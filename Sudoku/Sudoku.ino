ec966ecd7ee518597686e72bf3d02d7655174425
Erwin Bonsma <eriban@users.sourceforge.net>
2018-11-05 23:51:30 +0100

    Further improved solve animation
    Color of lights is now not based on single column but on weighted average of two columns. This increased the range of possible colors from four to twelve, thereby making the changes more gradual.
    
    Furthermore, added extra color variation to scrolling text by changing "done" to light green and exclamation mark to yellow.

--- Sudoku/Sudoku.ino
+++ Sudoku/Sudoku.ino
@@ -86,12 +86,27 @@ void setup() {
 #ifdef DEVELOPMENT
   initDebugLog();
 
-  int gradientLightColors[8] = {
-    0x000000, 0x504d01, 0xa09a02, 0xf1e703,
-    0x000000, 0x293e55, 0x537caa, 0x7dbbff
+  int gradientLightColors[39] = {
+    // Yellow
+    0x000000, 0x141300, 0x282600, 0x3c3900,
+    0x504d01, 0x646001, 0x787301, 0x8c8601,
+    0xa09a02, 0xb4ad02, 0xc8c002, 0xdcd302,
+    0xf1e703,
+
+    // Light blue
+    0x000000, 0x0a0f15, 0x141f2a, 0x1f2e3f,
+    0x293e55, 0x344d6a, 0x3e5d7f, 0x486d94,
+    0x537caa, 0x5d8cbf, 0x689bd4, 0x72abe9,
+    0x7dbbff,
+
+    // Light green
+    0x000000, 0x0b1107, 0x16220e, 0x213316,
+    0x2c451d, 0x375624, 0x42672c, 0x4d7833,
+    0x588a3a, 0x639b42, 0x6eac49, 0x79bd50,
+    0x85cf58
   };
 
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < 39; i++) {
     int hex = gradientLightColors[i];
     Color gbc = gb.createColor((hex >> 16) & 255, (hex >> 8) & 255, hex & 255);
     SerialUSB.printf("0x%04x, ", gbc);
